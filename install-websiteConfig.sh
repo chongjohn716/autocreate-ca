fd404341b1e5c8cc543311c7813a18c2d30f25f2
chong.john716 <chong.john716@gmail.com>
2018-04-20 21:06:14 +0800

    ubuntu no /root/ca issue

--- install-websiteConfig.sh
+++ install-websiteConfig.sh
@@ -1,16 +1,16 @@
 cd /root/ca;
 openssl genrsa -aes256 \
-      -out intermediate/private/www.barretlee.com.key.pem 2048;
-chmod 400 intermediate/private/www.barretlee.com.key.pem;
+      -out intermediate/private/www.vue-pwa-demo.com.key.pem 2048;
+chmod 400 intermediate/private/www.vue-pwa-demo.com.key.pem;
 openssl req -config intermediate/openssl.cnf \
-      -key intermediate/private/www.barretlee.com.key.pem \
-      -new -sha256 -out intermediate/csr/www.barretlee.com.csr.pem;
+      -key intermediate/private/www.vue-pwa-demo.com.key.pem \
+      -new -sha256 -out intermediate/csr/www.vue-pwa-demo.com.csr.pem;
 openssl ca -config intermediate/openssl.cnf \
       -extensions server_cert -days 375 -notext -md sha256 \
-      -in intermediate/csr/www.barretlee.com.csr.pem \
-      -out intermediate/certs/www.barretlee.com.cert.pem;
-chmod 444 intermediate/certs/www.barretlee.com.cert.pem;
+      -in intermediate/csr/www.vue-pwa-demo.com.csr.pem \
+      -out intermediate/certs/www.vue-pwa-demo.com.cert.pem;
+chmod 444 intermediate/certs/www.vue-pwa-demo.com.cert.pem;
 openssl x509 -noout -text \
-      -in intermediate/certs/www.barretlee.com.cert.pem;
+      -in intermediate/certs/www.vue-pwa-demo.com.cert.pem;
 openssl verify -CAfile intermediate/certs/ca-chain.cert.pem \
-      intermediate/certs/www.barretlee.com.cert.pem;
+      intermediate/certs/www.vue-pwa-demo.com.cert.pem;
